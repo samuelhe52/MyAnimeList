44df314d0d58475afc06de9d41cd53285b38835e
Samuel He <samuelhe52@outlook.com>
2025-06-27 16:54:22 +0800

    Add files for data storage v2.3.1

---
+++ DataProvider/Sources/DataProvider/Models/V2/SchemaV2_3_1.swift
@@ -0,0 +1,19 @@
+//
+//  SchemaV2_3_1.swift
+//  MyAnimeList
+//
+//  Created by Samuel He on 2025/6/27.
+//
+
+import Foundation
+import SwiftData
+
+public enum SchemaV2_3_1: VersionedSchema {
+    public static var versionIdentifier: Schema.Version {
+        .init(2, 3, 1)
+    }
+    
+    public static var models: [any PersistentModel.Type] {
+        [AnimeEntry.self]
+    }
+}

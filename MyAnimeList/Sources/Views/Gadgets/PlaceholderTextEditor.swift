6f39b1cfc2b067b13cf560e69c32172c439e63a6
Samuel He <samuelhe52@outlook.com>
2025-06-28 17:36:38 +0800

    Allowed basic note taking

---
+++ MyAnimeList/Sources/Views/Gadgets/PlaceholderTextEditor.swift
@@ -0,0 +1,27 @@
+//
+//  PlaceholderTextEditor.swift
+//  MyAnimeList
+//
+//  Created by Samuel He on 2025/6/16.
+//
+
+import SwiftUI
+
+struct PlaceholderTextEditor: View {
+    @Binding var text: String
+    var placeholder: LocalizedStringKey
+
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundStyle(.tertiary)
+                    .padding(.top, 8)
+                    .padding(.leading, 4)
+            }
+            TextEditor(text: $text)
+        }
+        .frame(width: nil, height: nil)
+        .scrollContentBackground(.hidden)
+    }
+}
